981abe351881a2eb5bb97aee937fbfadaa5cc20f
dogg <doggybay12@gmail.com>
2023-09-02 10:27:16 -0700

    working up to dto

--- ToDoWebApi/Controllers/TodoItemsController.cs
+++ ToDoWebApi/Controllers/TodoItemsController.cs
@@ -9,7 +9,7 @@ using TodoApi.Models;
 
 namespace ToDoWebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
